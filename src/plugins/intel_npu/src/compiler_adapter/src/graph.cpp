cdd9fb85d7709d7834b3ebb09490fe97529ee6f8
Nikita Kudriavtsev <nikita.kudriavtsev@intel.com>
2025-05-13 13:37:18 +0000

    Print correct blob size for CiD and more logs

--- src/plugins/intel_npu/src/compiler_adapter/src/graph.cpp
+++ src/plugins/intel_npu/src/compiler_adapter/src/graph.cpp
@@ -66,7 +66,7 @@ size_t Graph::export_blob(std::ostream& stream) const {
         }
 
         std::stringstream str;
-        str << "Blob size: " << blobSize << ", hash: " << std::hex << result;
+        str << "Part of blob size: " << blobSize << ", hash: " << std::hex << result;
         _logger.info(str.str().c_str());
     }
     _logger.info("Write blob to stream successfully.");
